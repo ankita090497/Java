5b2e3b5065c9c30680b7d74009c435ce801906cc
ankita <ankita@localhost>
2020-03-16 11:29:32 +0530

    "Employee"

--- src/com/pojo/Employee.java
+++ src/com/pojo/Employee.java
@@ -3,4 +3,19 @@ package com.pojo;
 public class Employee {
 	private String Name;
 	private String username;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
 }
